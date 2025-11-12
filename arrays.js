bb9e30522971cada20f60049e7d535e094401b5e
kaushlendra singh <thakurkaushlendra32@gmail.com>
2025-11-12 09:15:09 +0530

    first commit

---
+++ arrays.js
@@ -0,0 +1,32 @@
+const myarr = [1, 2, 3, 4, 5];
+// console.log(myarr[4]);
+// arrays methods
+// myarr.push(8);
+// console.log(myarr);
+// myarr.pop();
+// myarr.unshift(8);
+// myarr.shift();
+
+// console.log(myarr.includes(8));
+// console.log(myarr.indexOf(3));
+// console.log(myarr);
+
+// const newarr = myarr.join();
+
+// console.log(myarr);
+// console.log(typeof newarr);
+
+
+// slice and splice
+
+
+// console.log("A " ,myarr);
+// const newarr = myarr.slice(1,4);
+// console.log(newarr);
+// console.log("B " ,myarr);
+
+
+
+// const nnewarr2 = myarr.splice(1,3);
+// console.log("c " ,myarr);
+// console.log(nnewarr2);
